470244c6c4eb4227af0f85474bc7d09ea7f19234
richard <richard@gurustu.co>
2014-08-27 09:43:35 -0400

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    <title>GuRuStu Boilerplate</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" type="text/css" href="./css/app.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script src="//code.jquery.com/jquery-1.11.1.js"></script>
+</head>
+<body>
+<div class="container">
+
+
+
+
+</div> <!-- end .container -->
+<script src="./js/app.js"></script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,3 @@
+$(function(){
+	console.log( 'Do you know what is “up dog”? Our servers. We like to keep it that way too, server familiarity is a plus.' );
+});
