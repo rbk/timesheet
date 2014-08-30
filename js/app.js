8bb201b89b5be78b020a3dca0f99bd06d59900e7
richard <richard.be.jamin@gmail.com>
2014-08-30 11:38:00 -0400

    insert sample data to indexed db

--- js/app.js
+++ js/app.js
@@ -6,7 +6,68 @@ $(function(){
 	if (!window.indexedDB) {
 	    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
 	}
+
+
+
+
+
+
+	// This is what our customer data looks like.
+const customerData = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+];
+
+const dbName = "the_name";
+
+var request = indexedDB.open(dbName, 2);
+
+request.onerror = function(event) {
+  // Handle errors.
+};
+request.onupgradeneeded = function(event) {
+  var db = event.target.result;
+
+  // Create an objectStore to hold information about our customers. We're
+  // going to use "ssn" as our key path because it's guaranteed to be
+  // unique - or at least that's what I was told during the kickoff meeting.
+  var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+  // Create an index to search customers by name. We may have duplicates
+  // so we can't use a unique index.
+  objectStore.createIndex("name", "name", { unique: false });
+
+  // Create an index to search customers by email. We want to ensure that
+  // no two customers have the same email, so use a unique index.
+  objectStore.createIndex("email", "email", { unique: true });
+
+  // Use transaction oncomplete to make sure the objectStore creation is 
+  // finished before adding data into it.
+  objectStore.transaction.oncomplete = function(event) {
+    // Store values in the newly created objectStore.
+    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+    for (var i in customerData) {
+      customerObjectStore.add(customerData[i]);
+    }
+  }
+};
+
+
+
+
+
+
+
+	var db;
 	var request = window.indexedDB.open("gurustu_timesheets", 1);
-	console.log( request )
+
+	request.onerror = function(event) {
+		alert("Why didn't you allow my web app to use IndexedDB?!");
+	};
+	
+	request.onsuccess = function(event) {
+		db = request.result;
+		console.log( db )	
+	};
 
 });
