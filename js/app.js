51232f8624d4f2a83a23468e10af04ba224542d4
richard <richard.be.jamin@gmail.com>
2014-08-30 15:14:11 -0400

    settings working!

--- js/app.js
+++ js/app.js
@@ -31,20 +31,32 @@ $(function(){
             ] 
         }
     ];
+    var settings = [
+        {
+            name: 'Guest123',
+            email: 'guest123@gurustu.co',
+            theme: 'dark'
+        }
+    ];
     if(idbSupported) {
 
         var db;
         var db_name = 'timesheets';
         var transaction, store;
+        var request = indexedDB.open(db_name,7);
         
-        var request = indexedDB.open(db_name,4);
         request.onupgradeneeded = function(e) {
             console.log( 'Upgrading Database!' );
             var thisDB = e.target.result;
 
             if(!thisDB.objectStoreNames.contains("timesheets")) {
-                thisDB.createObjectStore("timesheets",{ key: "name" });
+                thisDB.createObjectStore("timesheets",{ keyPath: "name" });
                 // thisDb.createObjectStore("test2", { autoIncrement: true });
+            }
+            if(!thisDB.objectStoreNames.contains("settings")) {
+                var objectStore = thisDB.createObjectStore("settings",{ keyPath: "email" });
+                objectStore.createIndex("email", "email", { unique: true });
+                objectStore.createIndex("name", "name", { unique: true });
             }
         };
         request.onerror = function(e) {
@@ -53,16 +65,43 @@ $(function(){
         request.onsuccess = function(e) {
             db = e.target.result;
             console.log( 'Connected to database: "' + db_name + '"');
-            saveTimesheet()
+            saveTimesheet();
+            init();
+            getSettings( e.target.result );
         };
         function saveTimesheet(){
             var transaction = db.transaction(['timesheets'],'readwrite');
             var store = transaction.objectStore('timesheets');
-            var request = store.add(employeeTimesheets,1);
+            var request = store.add(employeeTimesheets[0]);
             request.onsuccess = function(e){
                 console.log('Saved');
             }
         }
+        function init(){
+            var transaction = db.transaction(['settings'],'readwrite');
+            var store = transaction.objectStore('settings');
+            var request = store.add(settings[0]);
+            request.onsuccess = function(e){
+                console.log('Saved');
+                console.log(e);
+            }
+        }
+        function getSettings(db){
+            // console.log( settings );
+            var transaction = db.transaction(["settings"]);
+            var objectStore = transaction.objectStore("settings");
+
+            // Use get() to get a specific object from the object store, the key of which is "Walk dog"
+            var request = objectStore.get("Guest123");
+            request.onerror = function(event) {
+            console.log("There is no record stored for " + request.result.taskTitle);
+            };
+            request.onsuccess = function(event) {
+            // Do something with the request.result!
+                console.log( request.result )
+            };
+
+        }   
     
     };
 
