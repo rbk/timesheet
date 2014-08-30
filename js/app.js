e9b6b517a3d937b29579cf0d057bb47dfe7840db
richard <richard.be.jamin@gmail.com>
2014-08-30 13:56:55 -0400

    working on indexeddb

--- js/app.js
+++ js/app.js
@@ -1,73 +1,57 @@
 $(function(){
+    
+    var idbSupported = false;
 
 	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
 	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
 	if (!window.indexedDB) {
 	    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
-	}
-
-
-
-
-
-
-	// This is what our customer data looks like.
-const customerData = [
-  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-];
-
-const dbName = "the_name";
-
-var request = indexedDB.open(dbName, 2);
-
-request.onerror = function(event) {
-  // Handle errors.
-};
-request.onupgradeneeded = function(event) {
-  var db = event.target.result;
-
-  // Create an objectStore to hold information about our customers. We're
-  // going to use "ssn" as our key path because it's guaranteed to be
-  // unique - or at least that's what I was told during the kickoff meeting.
-  var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-  // Create an index to search customers by name. We may have duplicates
-  // so we can't use a unique index.
-  objectStore.createIndex("name", "name", { unique: false });
-
-  // Create an index to search customers by email. We want to ensure that
-  // no two customers have the same email, so use a unique index.
-  objectStore.createIndex("email", "email", { unique: true });
-
-  // Use transaction oncomplete to make sure the objectStore creation is 
-  // finished before adding data into it.
-  objectStore.transaction.oncomplete = function(event) {
-    // Store values in the newly created objectStore.
-    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-    for (var i in customerData) {
-      customerObjectStore.add(customerData[i]);
+	} else {
+        var idbSupported = true;
     }
-  }
-};
 
 
+    var random = Math.floor(Math.random()*100); console.log( 'Lucky Number: ' + random );
 
+    var employeeTimesheets = [
+        { 
+            id: 'autoincrement ID', 
+            name: 'employee name', 
+            date: '08/30/2014',
+                work: [
+                { 
+                    company: 'Company Name',
+                    totalHours: 3.5
+                },
+                { 
+                    company: 'Another Company Name',
+                    totalHours: 1.5
+                }
+            ] 
+        }
+    ];
+    if(idbSupported) {
 
+        var db;
+        var db_name = 'timesheets';
+        
+        var request = indexedDB.open(db_name,1);
+        request.onupgradeneeded = function(event) {
+            console.log( 'Upgrading Database!' );
+        };
+        request.onerror = function(event) {
+            console.log( 'Something went wrong whilest connection to db...' );
+        };
+        request.onsuccess = function(event) {
+            console.log( 'Connected to database: "' + db_name + '"');
+            console.log( event.target.result );
+        };
+    
+    };
 
-
-
-	var db;
-	var request = window.indexedDB.open("gurustu_timesheets", 1);
-
-	request.onerror = function(event) {
-		alert("Why didn't you allow my web app to use IndexedDB?!");
-	};
-	
-	request.onsuccess = function(event) {
-		db = request.result;
-		console.log( db )	
-	};
 
 });
+
+
+
